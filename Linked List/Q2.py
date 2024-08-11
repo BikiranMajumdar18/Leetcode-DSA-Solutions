c309419326c44bab532921632fe8bfc1e3294c0e
BikiranMajumdar18 <163332413+BikiranMajumdar18@users.noreply.github.com>
2024-08-11 19:13:31 +0530

    Create Q2.py

---
+++ Linked List/Q2.py
@@ -0,0 +1,11 @@
+# Count number of Linked List nodes
+
+class Solution:
+    def constructLL(self, arr):
+        # code here
+        head = Node(arr[0])
+        temp = head
+        for i in range(1, len(arr)):
+            temp.next = Node(arr[i])
+            temp = temp.next
+        return head
